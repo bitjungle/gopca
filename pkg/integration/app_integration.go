9f54be71a6262da08c893e4cee3cc3469a42b424
Rune Mathisen <devel@bitjungle.com>
2025-08-10 20:54:20 +0200

    refactor: remove dead OpenURL function from integration package
    - Function was not used anywhere in the codebase
    - Both apps use Wails' BrowserOpenURL instead
    - Reduces code maintenance burden

--- pkg/integration/app_integration.go
+++ pkg/integration/app_integration.go
@@ -379,21 +379,3 @@ func IsAppRunning(appName string) bool {
 		return err == nil
 	}
 }
-
-// OpenURL opens a URL in the default browser (platform-independent)
-func OpenURL(url string) error {
-	var cmd *exec.Cmd
-
-	switch runtime.GOOS {
-	case "darwin":
-		cmd = exec.Command("open", url)
-	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
-	case "linux":
-		cmd = exec.Command("xdg-open", url)
-	default:
-		return fmt.Errorf("unsupported platform: %s", runtime.GOOS)
-	}
-
-	return cmd.Start()
-}
